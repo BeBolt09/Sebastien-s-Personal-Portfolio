efbcdd2f83fad6dcdfab8bd9f23a48439e2f1b43
Sebastien Dupont <sebastiengdupont@gmail.com>
2024-02-01 12:41:22 -0800

    Updated some new icons
    Finish Contact section
    Next :
    -copyright section
    -Fill in Lorem in Portfolio
    -Create Portfolio dropdown
    -Create About me new page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -20,30 +20,42 @@ const form = useRef<HTMLFormElement>(null);
             <div className='font-mono text-3xl text-gray-600'>Contact</div> 
             <img src="/mail.svg" className='ml-3 h-10 w-auto'/>
         </div>
-        <form ref={form} onSubmit={sendEmail} className='max-w-md mx-auto p-14 m-14 border-2 border-gray-600 rounded-lg flex flex-col'>
-            <div className='flex justify-center mb-5'>
-                <div className='mx-2 flex-1'>
-                    <label><p className='text-gray-600 text-bold font-mono text-xl text-left'>Name</p></label>
-                    <input type="text" name="user_name" className='w-full border-2 border-gray-600 rounded'/>
-                </div>
-                <div className='mx-2 flex-2'>
-                    <label><p className='text-gray-600 text-bold font-mono text-xl text-left'>Email</p></label>
-                    <input type="email" name="user_email" className='w-full border-2 border-gray-600 rounded'/>
+
+        <div className='flex items-center'>
+            <div className='max-w-md mx-auto m-14 border-2 border-gray-600 rounded-lg'>
+                <p className='text-2xl font-mono text-gray-600 p-10'>Find me here!</p>
+                <div className='max-w-md mx-auto m-14 border-2'>
+                    <p className='flex justify-center'><a href="https://linkedin.com"><img src="/linkedin.svg" className='color:text-blue-600 h-10 w-auto'/></a></p>
+                    <p className='flex justify-center'><a href="https://youtube.com"><img src="/Youtube.svg" className='color:text-blue-600 h-10 w-auto'/></a></p>
+                    <p className='flex justify-center'><a href="sebastiengdupont@gmail.com"><img src="/Email.svg" className='color:text-blue-600 h-10 w-auto'/></a></p>
                 </div>
             </div>
-            <div className='mx-2 mb-5'>
-                <label><p className='text-gray-600 text-bold font-mono text-xl text-left'>Message</p></label>
-                <textarea name="message" className='h-24 w-full border-2 border-gray-600 rounded'/>
-            </div>
-            <div className='mt-5'>
-                <button type="submit" className='py-2 px-4 rounded-lg border-2 border-gray-600 hover:font-bold'>
-                    <div className='flex'>
-                        <p className='text-gray-600 font-mono hover:text-bold '>Send</p>
-                        <img src="/send.svg" className='h-6 w-auto'/>
+
+            <form ref={form} onSubmit={sendEmail} className='max-w-md mx-auto p-14 m-14 border-2 border-gray-600 rounded-lg flex flex-col'>
+                <div className='flex justify-center mb-5'>
+                    <div className='mx-2 flex-1'>
+                        <label><p className='text-gray-600 text-bold font-mono text-xl text-left'>Name</p></label>
+                        <input type="text" name="user_name" className='w-full border-2 border-gray-600 rounded'/>
                     </div>
-                </button>
-            </div>
-        </form>
+                    <div className='mx-2 flex-2'>
+                        <label><p className='text-gray-600 text-bold font-mono text-xl text-left'>Email</p></label>
+                        <input type="email" name="user_email" className='w-full border-2 border-gray-600 rounded'/>
+                    </div>
+                </div>
+                <div className='mx-2 mb-5'>
+                    <label><p className='text-gray-600 text-bold font-mono text-xl text-left'>Message</p></label>
+                    <textarea name="message" className='h-24 w-full border-2 border-gray-600 rounded'/>
+                </div>
+                <div className='mt-5'>
+                    <button type="submit" className='py-2 px-4 rounded-lg border-2 border-gray-600 hover:font-bold'>
+                        <div className='flex'>
+                            <p className='text-gray-600 font-mono hover:text-bold '>Send</p>
+                            <img src="/send.svg" className='h-6 w-auto'/>
+                        </div>
+                    </button>
+                </div>
+            </form>
+        </div>
     </>
   );
 };
