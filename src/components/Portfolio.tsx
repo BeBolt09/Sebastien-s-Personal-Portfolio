a50ee6dee7222f1321e9863afd070a6b59010720
Sebastien Dupont <sebastiengdupont@gmail.com>
2024-02-19 12:35:25 -0800

    Added new projects
    added two portfolio projecgss

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -27,17 +27,37 @@ export default function Portfolio() {
         {/* <div className='flex lg:flex-row sm:flex-col items-center justify-center mt-5 '> */}
         {/* <div className='flex flex-col lg:flex-row md:flex-col sm:flex-col items-center justify-center mt-5'> */}
         <div className='grid lg:grid-cols-3 md:grid-cols-1 place-items-center gap-5 mt-5'>
-            <button onClick={handleSeePopupClick}>
             <div className="max-w-sm rounded-lg overflow-hidden border-2 border-gray-500 shadow-lg mx-5">
-                <img className="w-full p-4" src='/chrome-extension.png'/>
+                <video autoPlay loop muted playsInline src="/Lesson Planner Demo .mp4" className="rounded w-full px-4 pt-10 pb-9"/>
                 <div className="px-6 pb-4">
-                    <div className="font-mono font-bold text-gray-600 text-xl mb-2">Chrome Extension</div>
+                    <div className="font-mono font-bold text-gray-600 text-xl mb-2">Lesson Planner</div>
                     <p className="font-mono text-gray-600 text-base text-left">
-                        Chrome extension where users can get easy understanding of their browser productivity through visualizations, graphs, meaningful stats.
+                        I created a Lesson Planner application that let's anyone generate lesson plans with AI by entering subject, grade level and time frame.
                     </p>
                 </div>
             </div>
+            <button onClick={handleSeePopupClick}>
+                <div className="max-w-sm rounded-lg overflow-hidden border-2 border-gray-500 shadow-lg mx-5">
+                    <img className="w-full p-4" src='/chrome-extension.png'/>
+                    <div className="px-6 pb-4">
+                        <div className="font-mono font-bold text-gray-600 text-xl mb-2">Chrome Extension</div>
+                        <p className="font-mono text-gray-600 text-base text-left">
+                            Chrome extension where users can get easy understanding of their browser productivity through visualizations, graphs, meaningful stats.
+                        </p>
+                    </div>
+                </div>
             </button>
+            <div className="max-w-sm rounded-lg overflow-hidden border-2 border-gray-500 shadow-lg mx-5">
+                <img className="w-full p-4" src='/RecipeMaker.png'/>
+                <div className="px-6 pb-4">
+                    <div className="font-mono font-bold text-gray-600 text-xl mb-2">Recipe Generator</div>
+                    <p className="font-mono text-gray-600 text-base text-left">
+                        Don't know what to make with what's in your fridge? Just open up this recipe generator, enter the ingredients you have, and find out what you can make!
+                    </p>
+                </div>
+            </div>
+            {showMore && (
+            <>
             <div className="max-w-sm rounded-lg overflow-hidden border-2 border-gray-500 shadow-lg mx-5">
                 <img className="w-full p-4" src='/RandomeGallerypix.png'/>
                 <div className="px-6 pb-4">
@@ -53,17 +73,6 @@ export default function Portfolio() {
                     <div className="font-mono font-bold text-gray-600 text-xl mb-2">Contact Tracker</div>
                     <p className="font-mono text-gray-600 text-base text-left">
                         Web App that allows you to create, retrieve, update and delete contacts. Save the Name, twitter account, image and notes on someone.
-                    </p>
-                </div>
-            </div>
-            {showMore && (
-            <>
-            <div className="max-w-sm rounded-lg overflow-hidden border-2 border-gray-500 shadow-lg mt-5 mx-5 ">
-                <img className="w-full pt-8 pb-16 px-4" src='/WorkInProgress.jpg'/>
-                <div className="px-6 pb-4">
-                    <div className="font-mono font-bold text-gray-600 text-xl mb-2">In Progress</div>
-                    <p className="font-mono text-gray-600 text-base text-left">
-                        Exciting project for roomates incoming. Check back on 02/20
                     </p>
                 </div>
             </div>
